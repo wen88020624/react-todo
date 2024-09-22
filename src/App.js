72f6125d124ceb9af7553748d63a494edf07d373
wen88020624 <wen880206@gmail.com>
2024-09-22 16:24:00 +0800

    update task checkbox

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Todo from './components/Todo.jsx';
 import Form from './components/Form.jsx';
 import FilterButton from './components/FilterButton.jsx';
 
+const FILTER_MAP = {
+  All: () => true,
+  Done: task => task.completed,
+  Active: task => !task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
+  const [filter, setFilter] = useState('All');
+  const [tasks, setTasks] = useState(props.tasks || []);
 
-  const taskList = props.tasks?.map((task) => (
-    <Todo 
-      id={task.id} 
-      name={task.name} 
-      completed={task.completed} 
-      key={task.id}
-    />)
-  );
+  const toggleTaskCompleted = (id) => {
+    const updatedTasks = tasks.map(task =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const taskList = tasks
+    ?.filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo 
+        id={task.id} 
+        name={task.name} 
+        completed={task.completed} 
+        key={task.id}
+        toggleCompleted={toggleTaskCompleted}
+      />
+    ));
 
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1>To-do List</h1>
       <Form />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {FILTER_NAMES.map(name => (
+          <FilterButton 
+            key={name} 
+            label={name} 
+            isPressed={name === filter}
+            setFilter={setFilter}
+          />
+        ))}
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {taskList.length} tasks remaining
       </h2>
       <ul
         role="list"
@@ -34,10 +59,8 @@ function App(props) {
         aria-labelledby="list-heading">
         {taskList}
       </ul>
-
     </div>
   );
 }
-
 
 export default App;
