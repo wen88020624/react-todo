b4ab2dec5fb62af5776ddba77309652eac2f5a66
wen88020624 <wen880206@gmail.com>
2024-09-22 16:25:31 +0800

    fix filter button

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,12 +1,18 @@
-function FilterButton() {
-    return (
-      <button type="button" className="btn toggle-btn" aria-pressed="true">
-        <span className="visually-hidden">Show </span>
-        <span>all </span>
-        <span className="visually-hidden"> tasks</span>
-      </button>
-    );
-  }
-  
-  export default FilterButton;
-  
+import React from 'react';
+
+function FilterButton(props) {
+  return (
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.label)}
+    >
+      <span className="visually-hidden">Show </span>
+      <span>{props.label}</span>
+      <span className="visually-hidden"> tasks</span>
+    </button>
+  );
+}
+
+export default FilterButton;
